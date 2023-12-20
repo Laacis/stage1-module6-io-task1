d29d0fe596884ef529f263c6ec3b571c52754a63
Jurijs <j.lacis.code@gmail.com>
2023-12-20 10:09:06 +0800

    fixing quality

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -6,36 +6,37 @@ import java.io.*;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
+        try (InputStream inputStream = new FileInputStream(file)) {
+            return getProfile(inputStream);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return  new Profile();
+    }
+
+    private static Profile getProfile(InputStream inputStream) throws IOException {
         String name = "";
         String email = "";
         int age = 0;
         long phone = 0;
 
-
-        try (InputStream inputStream = new FileInputStream(file)) {
-            try(BufferedReader reader = new BufferedReader((new InputStreamReader(inputStream)))){
-                String line;
-                while((line = reader.readLine()) != null){
-                    String[] tempStr = line.split(" ");
-                    if(tempStr[0].contains("Name:")){
-                        name = tempStr[1];
-                    } else if (tempStr[0].contains("Age:")) {
-                        age = Integer.parseInt(tempStr[1]);
-                    }else if (tempStr[0].contains("Email:")) {
-                        email = tempStr[1];
-                    }else if (tempStr[0].contains("Phone:")) {
-                        phone = Long.parseLong(tempStr[1]);
-                    }
+        try(BufferedReader reader = new BufferedReader((new InputStreamReader(inputStream)))){
+            String line;
+            while((line = reader.readLine()) != null){
+                String[] tempStr = line.split(" ");
+                if(tempStr[0].contains("Name:")){
+                    name = tempStr[1];
+                } else if (tempStr[0].contains("Age:")) {
+                    age = Integer.parseInt(tempStr[1]);
+                }else if (tempStr[0].contains("Email:")) {
+                    email = tempStr[1];
+                }else if (tempStr[0].contains("Phone:")) {
+                    phone = Long.parseLong(tempStr[1]);
                 }
-            }catch (IOException e){
-                throw new IOException(e);
-            }finally {
-                inputStream.close();
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            return new Profile(name, age, email, phone);
+        }catch (IOException e){
+            throw new IOException(e);
         }
-
-        return new Profile(name, age, email, phone);
     }
 }
